4941a63fd454a6f9f828e1020268fdcc28094703
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-10-05 10:34:56 -0300

    sitios mob

--- src/components/SitiosDeInteres.js
+++ src/components/SitiosDeInteres.js
@@ -27,36 +27,36 @@ const dropClosed = "drop-closed"
         < ul onMouseLeave={toggleClosed} className={isOpen ? dropOpen : dropClosed}>
             <div className="primeros-7">
 
-          <li className="drop-item">
+          <li className="drop-item-interes">
             <Link className="drop-link-interes" target="_blank" to="http://www.centrodeoficiales.com.ar/" >
         Centro de Oficiales
             </Link>
             </li>
             
-          <li className="drop-item">
+          <li className="drop-item-interes">
             <Link className="drop-link-interes" target="_blank" to="https://www.centronaval.org.ar/">
             Centro Naval
             </Link>
             </li>
-          <li className="drop-item">
+          <li className="drop-item-interes">
             <Link className="drop-link-interes" target="_blank" to="https://www.argentina.gob.ar/ejercito">
             Ejercito Argentino
             </Link>
             
             </li>
          
-          <li className="drop-item">
+          <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://mupim.org.ar/">
           Mupim
             </Link>
             </li>
-          <li className="drop-item"> 
+          <li className="drop-item-interes"> 
           <Link className="drop-link-interes" target="_blank" to="https://femfase.com.ar/">
           FEMFASE
           </Link>
           </li>
           
-          <li className="drop-item">
+          <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://www.smsv.com.ar/">
             Sociedad Militar Seguro de Vida
             </Link>
@@ -64,32 +64,32 @@ const dropClosed = "drop-closed"
             </div>
 
             <div className="segundos-6">
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="http://www.circulomilitar.org/">
+            <li className="drop-item-interes">
+          <Link  className="drop-link-interes" target="_blank" to="http://www.circulomilitar.org/">
             Circulo Militar
             </Link>
             </li>
-            <li className="drop-item">
+            <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://www.argentina.gob.ar/iaf">
             Instituto de Ayuda Financiera 
             </Link>
             </li>
-            <li className="drop-item">
+            <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://capellaniamayorea.ejercito.mil.ar/">
            Capellania Mayor del Ejercito
             </Link>
             </li>
-            <li className="drop-item">
+            <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://cirgen.org.ar/">
            Circulo de Gendarmeria Nacional
             </Link>
             </li>
-            <li className="drop-item">
+            <li className="drop-item-interes">
           <Link className="drop-link-interes" target="_blank" to="https://www.ampgn.com.ar/AMUGENAL/">
            Ayuda mutua del personal Gendarmeria Nacional
             </Link>
             </li>
-            <li className="drop-item">
+            <li className="drop-item-interes">
             <Link className="drop-link-interes" target="_blank" to="https://www.aca.org.ar/">
             Automovil Club Argentino
             </Link>
--- src/components/SitiosMobile.js
+++ src/components/SitiosMobile.js
@@ -5,73 +5,73 @@ import '../styles/SitiosMobile.css'
 const SitiosMobile = () => {
   return (
     <main className="sitios-mob">
-          < ul  >
+          <ul>
         
 
-          <li className="drop-item">
-            <Link className="drop-link-interes" target="_blank" to="http://www.centrodeoficiales.com.ar/" >
+          <li className="drop-item-mob">
+            <Link className="drop-link-mob" target="_blank" to="http://www.centrodeoficiales.com.ar/" >
         Centro de Oficiales
             </Link>
             </li>
             
-          <li className="drop-item">
-            <Link className="drop-link-interes" target="_blank" to="https://www.centronaval.org.ar/">
+          <li className="drop-item-mob">
+            <Link className="drop-link-mob" target="_blank" to="https://www.centronaval.org.ar/">
             Centro Naval
             </Link>
             </li>
-          <li className="drop-item">
-            <Link className="drop-link-interes" target="_blank" to="https://www.argentina.gob.ar/ejercito">
+          <li className="drop-item-mob">
+            <Link className="drop-link-mob" target="_blank" to="https://www.argentina.gob.ar/ejercito">
             Ejercito Argentino
             </Link>
             
             </li>
          
-          <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://mupim.org.ar/">
+          <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://mupim.org.ar/">
           Mupim
             </Link>
             </li>
-          <li className="drop-item"> 
-          <Link className="drop-link-interes" target="_blank" to="https://femfase.com.ar/">
+          <li className="drop-item-mob"> 
+          <Link className="drop-link-mob" target="_blank" to="https://femfase.com.ar/">
           FEMFASE
           </Link>
           </li>
           
-          <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://www.smsv.com.ar/">
+          <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://www.smsv.com.ar/">
             Sociedad Militar Seguro de Vida
             </Link>
             </li>
           
 
   
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="http://www.circulomilitar.org/">
+            <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="http://www.circulomilitar.org/">
             Circulo Militar
             </Link>
             </li>
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://www.argentina.gob.ar/iaf">
+            <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://www.argentina.gob.ar/iaf">
             Instituto de Ayuda Financiera 
             </Link>
             </li>
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://capellaniamayorea.ejercito.mil.ar/">
+            <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://capellaniamayorea.ejercito.mil.ar/">
            Capellania Mayor del Ejercito
             </Link>
             </li>
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://cirgen.org.ar/">
+            <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://cirgen.org.ar/">
            Circulo de Gendarmeria Nacional
             </Link>
             </li>
-            <li className="drop-item">
-          <Link className="drop-link-interes" target="_blank" to="https://www.ampgn.com.ar/AMUGENAL/">
+            <li className="drop-item-mob">
+          <Link className="drop-link-mob" target="_blank" to="https://www.ampgn.com.ar/AMUGENAL/">
            Ayuda mutua del personal Gendarmeria Nacional
             </Link>
             </li>
-            <li className="drop-item">
-            <Link className="drop-link-interes" target="_blank" to="https://www.aca.org.ar/">
+            <li className="drop-item-mob">
+            <Link className="drop-link-mob" target="_blank" to="https://www.aca.org.ar/">
             Automovil Club Argentino
             </Link>
             </li>
