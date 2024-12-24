5d4059e60c2f6582a4df01ca7817cca5442583de
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-12-24 00:30:35 -0300

    add text

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
       <ScrollToTop />
 
      
-  {/*   <Modal
+     <Modal
         isOpen={modalIsOpen}
          contentLabel="Modal"
         style={{
@@ -95,173 +95,16 @@ function App() {
       padding: "20px"
     }
  }>
-  <h1 style={{
-    fontSize: "1.5rem",
-    color : "green"
-  }}>
-  Asociación Socorros Mutuos "Fuerzas Armadas"
-  </h1>
-  <h4 style={{
-    color: "green",
-    fontSize: "1rem"
-  }}>
-  ASAMBLEA ORDINARIA - Ejercicio Social 142º 
-  </h4>
-  <h4 style={{
-    color: "green",
-    fontSize: "1rem"
-  }}>
-  CONVOCATORIA
-  </h4>
   <p style={{
-    fontSize: ".85rem",
-    textAlign: "center",
-    margin: "10px",
-    padding: "10px"
-  }}>
-  En cumplimiento con lo establecido en las disposiciones legales y estatutarias el Consejo Directivo de la Asociación Socorros Mutuos “Fuerzas Armadas” convoca a los señores y señoras asociados Activos a participar de la Asamblea Ordinaria correspondiente al 142º Ejercicio, a realizarse el 12 de noviembre de 2024 a las 16.00 horas, en en Escuela Superior de Guerra “Teniente General Luis María Campos», sita en Av. Luis María Campos 480 de la Ciudad Autónoma de Buenos Aires (ingreso alternativo Savio 235), para tratar el siguiente 
-  </p>
-  <h4 style={{
-    color: "green",
-    fontSize: "1rem"
-  }}>
-  ORDEN DEL DÍA
-  </h4>
-  <ol style={{
-   width: "90%",
-    margin: "auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    fontSize: "1.6rem",
+    fontWeight: "bold",
+    color: "black",
+    padding: "20px",
+    textAlign: "center"
 
-  }}>
-    <li style={{
-      padding: "5px",
-      margin: "5px",
-      width: "40em",
-      fontSize: ".9rem",
-      textAlign: "start"
-
-
-    }}>
-    Designación de dos asociados para refrendar el Acta.
-    </li>
-    <li 
-    
-
-    style={{
-      padding: "5px",
-      margin: "5px",
-      width: "40em",
-      fontSize: ".9rem",
-      textAlign: "start"
-
-
-    }}
-   
-    >
-    Causas por las cuales la convocatoria se realizó fuera de término.
-    </li>
-    <li 
-    style={{
-      padding: "5px",
-      margin: "5px",
-      width: "40em",
-      fontSize: ".9rem",
-      textAlign: "start"
-
-
-    }}
->
-    Lectura y aprobación del Acta de la Asamblea Ordinaria anterior.
-    </li>
-   <li 
-   style={{
-    padding: "5px",
-    margin: "5px",
-    width: "40em",
-    fontSize: ".9rem",
-    textAlign: "start"
-
-
-  }}
-    >
-    Consideración de la Memoria, Inventario, Balance General, Estado de Recursos y Gastos, Estado de Flujo de Efectivo e informe de la Junta Fiscalizadora correspondiente al Ejercicio 142º finalizado el 30 de abril de 2024.
-    </li>
-    <li 
-
-style={{
-  padding: "5px",
-  margin: "5px",
-  width: "40em",
-  fontSize: ".9rem",
-  textAlign: "start"
-
-
-}}
-  >
-    Ratificación de las cuotas sociales y aranceles de servicios.
-    </li>
-    <li
-
-style={{
-  padding: "5px",
-  margin: "5px",
-  width: "40em",
-  fontSize: ".9rem",
-  textAlign: "start"
-
-
-}}
->
-    Ratificación de las retribuciones para los miembros del Consejo Directivo y de la Junta Fiscalizadora.
-    </li>
-    <li
-
-style={{
-  padding: "5px",
-  margin: "5px",
-  width: "40em",
-  fontSize: ".9rem",
-  textAlign: "start"
-
-
-}}
->
-    Elecciones
-    </li>
-  </ol>
-  <h5>
-  Ciudad Autónoma de Buenos Aires, 30 de agosto de 2024
-  </h5>
-  <div>
-    <p>
-    SERGIO RUBEN CHIACCHIO
-CORONEL (R)  
-SECRETARIO GENERAL 
-
-    </p>
-    <p>
-    JOSE LUIS BO
-CORONEL (R)
-PRESIDENTE
-</p>
-  </div>
-
-  <h5 style={{
-    color: "green",
-    fontSize: ".8rem"
-  }}>
-  ESTATUTO SOCIAL - DISPOSICION VIGENTE:
-  </h5>
-  <p style={{
-    fontSize: ".8rem",
-    textAlign: "center",
-    margin: "10px",
-    padding: "10px"
-  }}>
-  QUORUM - ARTÍCULO 34: El quórum para cualquier tipo de Asamblea, será de la mitad más uno de los asociados con derecho a participar. En caso de no alcanzar este número a la hora fijada la Asamblea podrá sesionar válidamente treinta minutos después con los asociados presentes, cuyo número no podrá ser menor al de los miembros de los órganos directivo y de fiscalización. 
+  }}>Desde el 16 Dic 24 al 16 Mar 25 la asociación Socorros Mutuos Fuerzas Armadas, realizará tareas entre las 0900 hrs hasta las 1300 hrs en Sede Central y panteones de Chacarita</p>
+  <p>
+  Consejo Directivo
   </p>
  </section>
 <div>
@@ -280,7 +123,7 @@ PRESIDENTE
  
        
   
-      </Modal>  */}
+      </Modal>  
 
 
       <Routes>
